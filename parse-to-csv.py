889c49148b35e173d372347141aaf7926031b581
Angel <lopez.angel@gmail.com>
2016-09-30 15:36:14 +0200

    unicode support

--- parse-to-csv.py
+++ parse-to-csv.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# Parse the transports files (json) into a csv file 
+# Parse the mm-output (json) into a csv file with a WKT field
 # Author: Angel J. Lopez @ UGent
 # Date: Sep 12, 2016
 # 
@@ -15,26 +15,29 @@
 import sys
 import json
 import csv
+import unicodecsv
 
 
 # read the file (parameter) or the standard input
 f = open(sys.argv[1]) if len(sys.argv) > 1 else sys.stdin    
 # write on the standard output
-f_w = csv.writer(sys.stdout)
+#f_w = csv.writer(sys.stdout)
+f_w=unicodecsv.writer(sys.stdout) # unicode support
 
 # waypoints is the default option
 op = sys.argv[2] if len(sys.argv)>2 else 'waypoints'
 datatype = op if op=='augmented_trajectory' else 'waypoints' #  'waypoints'
 
-count=0   # line reference
-header=0  # header flag
+count = 0
+header=0
 for line in f:
 	data = json.loads(line)
-	pt_cnt=0  # location count
+	pt_cnt = 0
 	count +=1 
 	for item in data[datatype]:
 		pt_cnt+=1	
-		row_value = item 
+		#row_value=None
+		row_value = item
 		row_value['atomid'] = data['atomid']
 		row_value['userid'] = data['userid']
 		row_value['starttime'] = data['startedAt']
@@ -42,8 +45,9 @@ for line in f:
 		row_value['mode'] = data['label']
 		row_value['loc_seq'] = pt_cnt
 		row_value['segment_seq'] = count
-
+		#print row_value
 		if header==0:
  			f_w.writerow(row_value.keys())
  			header=1
 		f_w.writerow(row_value.values())
+		
